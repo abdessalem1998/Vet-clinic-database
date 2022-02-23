1475ec43dd62d8b5ed18902aa3e6ba84b994e9c2
Houfaf abdessalem <abdeslam.dib.usa@gmail.com>
2022-02-23 22:41:19 +0100

    Insert the data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-
+/*Part 1 */
 CREATE TABLE animals(
    id              INT PRIMARY  KEY     NOT NULL,
    name            CHAR(50)    NOT NULL,
@@ -9,5 +9,6 @@ CREATE TABLE animals(
    weight_kg       DECIMAL
 );
 
+/*Part 2 */
 ALTER TABLE animals
 ADD COLUMN species CHAR(50);
